d55202d55795a88d44bb2fb33bce535fcce8f84d
RinSanom <rinsanom3@gmail.com>
2025-12-26 02:41:46 +0700

    fix: Fix Db connection in production

--- be/src/config/database.config.ts
+++ be/src/config/database.config.ts
@@ -9,7 +9,7 @@ class DBConfig {
       "mongodb://superuser:superuser@localhost:27017/theFashionAppDB?authSource=admin";
   }
 
-  public connectDB = async () => {
+  public async connectDB() {
     try {
       const options: any = {
         authSource: process.env.MONGO_AUTH_DB || "admin",
@@ -24,7 +24,7 @@ class DBConfig {
       console.error("Failed to connect to MongoDB:", err);
       process.exit(1);
     }
-  };
+  }
 }
 
 export default new DBConfig();
